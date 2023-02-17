22fae9339f6474219fdbeb449f8daf99d6c79d1e
Dagmara Grabowska <dagmarag0@gmail.com>
2023-02-17 10:23:08 +0100

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Art_2.0 JS imported successfully!");
+});
